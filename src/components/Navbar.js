82a2cc371c82ecdb2875d191150bbdbb72acb7cb
Demjanenko, Alexej <alexej.demjanenko@arithnea.de>
2020-03-07 00:10:04 +0100

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/ff.jpg'
+import logo from '../img/ff.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px', height: '88px', maxHeight: '88px' }} />
+              <img src={logo} alt="FRITZ FIT" style={{ width: '88px', height: '88px', maxHeight: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
